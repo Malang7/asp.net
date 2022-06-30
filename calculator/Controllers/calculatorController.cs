6bfdb42a6490a7f78ae405f0d77c9a527a4d7822
Matin Mohammadi <matinm@uia.no>
2022-07-01 01:17:57 +0200

    start

---
+++ calculator/Controllers/calculatorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace calculator.Controllers
+{
+    public class calculatorController : Controller
+    {
+        public IActionResult calculator()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ calculator/wwwroot/js/calculator.js
@@ -0,0 +1,43 @@
+﻿// declare som variables, it have to be global 
+
+let number; 
+let operator = ""; 
+let all  = "";  
+let array  = [];  
+
+
+$(document).ready(function() // main function to get ready the document 
+    {
+        
+        // this function display the numbers into the label  
+        $(".number").click(function (e)
+        {
+            // target get the DOM object:  if they are div, button etc
+            // value: get the value 
+            // i dont understand what e do here 
+
+            number  = e.target.value;
+                                       
+            if(operator != "")
+            {
+                all +=operator;                 // adding the opreator to the label  
+                operator = "";  
+            }
+            
+            all += number; // to have a 100 and more  numbers  
+            
+            $(".screen")[0].value =  all; // update the entry  
+           // why we do need to use [0] // what is the point of that? 
+            
+        })
+
+        $(".operator").click(function (e)
+        {
+            operator = e.target.value; // getting the value  
+            $(".screen")[0].value = all + operator;   // updating the entry  
+        })
+        
+       
+
+    }
+)
